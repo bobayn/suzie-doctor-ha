5b8a962b6a5ead14abec8d0daac0553eec93a49f
bobayn <boba.y.n@gmail.com>
2026-09-18 21:14:56 +0300

    Doctor: bump App and Protocol Pack for host probe

--- suzie_doctor/rootfs/app/suzie_doctor/__init__.py
+++ suzie_doctor/rootfs/app/suzie_doctor/__init__.py
@@ -1,5 +1,5 @@
 """Suzie Doctor App package."""
 
-APP_VERSION = "0.2.16-dev"
-PROTOCOL_PACK_VERSION = "0.1.1-dev"
+APP_VERSION = "0.2.17-dev"
+PROTOCOL_PACK_VERSION = "0.1.2-dev"
 BRIDGE_VERSION = "0.2.4-dev"
